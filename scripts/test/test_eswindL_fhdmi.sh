e7755df343c460b177806d709c6ecc0a887c934d
Karim Alaa <89037036+Karim19Alaa@users.noreply.github.com>
2025-09-04 18:03:54 +0300

    Update Readme.md (#1)

---
+++ scripts/test/test_eswindL_fhdmi.sh
@@ -0,0 +1,9 @@
+python eswindnet/test.py \
+name=001_train_WSESwinDNetL_Demoiring_UHDM_test \
+data=uhdm \
+model=eswindnet-l \
+testing=testing_uhdm \
+data.opt.train.dataset.dataroot="datasets/uhdm/train" \
+data.opt.val.dataset.dataroot="datasets/uhdm/test" \
+data.opt.test.dataset.dataroot="datasets/uhdm/test" \
+testing.trainer_args.ckpt_path= # {CHANGEME}
