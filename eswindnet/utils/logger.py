3827cc1972abdbf214c8bf32bc95964b4ecdcb4f
Karim Alaa <karimalaa1912@gmail.com>
2026-03-07 15:46:11 +0200

    Update logger

--- eswindnet/utils/logger.py
+++ eswindnet/utils/logger.py
@@ -5,11 +5,13 @@ Code from BasicSR: https://github.com/XPixelGroup/BasicSR
 import logging
 from omegaconf import OmegaConf
 import hydra
+
 # from .dist_utils import master_only
-from pytorch_lightning.utilities.rank_zero import rank_zero_only
+from lightning_fabric.utilities.rank_zero import rank_zero_only
 
 
 wandb_logger = None
+
 
 def get_wandb_logger():
     return wandb_logger
@@ -18,35 +20,43 @@ def get_wandb_logger():
 @rank_zero_only
 def init_tb_logger(save_dir):
     from pytorch_lightning.loggers import TensorBoardLogger
-    tb_logger = TensorBoardLogger(save_dir, name='')
+
+    tb_logger = TensorBoardLogger(save_dir, name="")
     return tb_logger
+
 
 @rank_zero_only
 def init_wandb_logger(opt):
     """We now only use wandb to sync tensorboard log."""
     import wandb
+
     logger = get_root_logger()
 
-    project = opt['logger']['wandb']['project']
-    resume_id = opt['logger']['wandb'].get('resume_id')
+    project = opt["logger"]["wandb"]["project"]
+    resume_id = opt["logger"]["wandb"].get("resume_id")
     if resume_id:
         wandb_id = resume_id
-        resume = 'allow'
-        logger.warning(f'Resume wandb logger with id={wandb_id}.')
+        resume = "allow"
+        logger.warning(f"Resume wandb logger with id={wandb_id}.")
     else:
         wandb_id = wandb.util.generate_id()
-        resume = 'never'
+        resume = "never"
+
+    wandb_logger = wandb.init(
+        id=wandb_id,
+        resume=resume,
+        name=opt["name"],
+        config=opt,
+        project=project,
+        sync_tensorboard=True,
+    )
+
+    logger.info(f"Use wandb logger with id={wandb_id}; project={project}.")
 
 
-    wandb_logger = wandb.init(id=wandb_id, resume=resume, name=opt['name'], config=opt, project=project, sync_tensorboard=True)
+def get_root_logger(logger_name="eswindnet"):
 
-    logger.info(f'Use wandb logger with id={wandb_id}; project={project}.')
-
-
-def get_root_logger(logger_name='eswindnet'):
-    
     return logging.getLogger(logger_name)
-
 
 
 def get_env_info():
@@ -58,12 +68,14 @@ def get_env_info():
     import torchvision
     import pytorch_lightning as pl
     import hydra
-    
-    msg = ('\nVersion Information: '
-            f'\n\tPyTorch: {torch.__version__}'
-            f'\n\tTorchVision: {torchvision.__version__}'
-            f'\n\tPyTorch-Lightning: {pl.__version__}'
-            f'\n\tHydra: {hydra.__version__}')
+
+    msg = (
+        "\nVersion Information: "
+        f"\n\tPyTorch: {torch.__version__}"
+        f"\n\tTorchVision: {torchvision.__version__}"
+        f"\n\tPyTorch-Lightning: {pl.__version__}"
+        f"\n\tHydra: {hydra.__version__}"
+    )
     return msg
 
 
@@ -72,18 +84,20 @@ def init_logging(cfg):
     opt = OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True)
 
     # initialize wandb logger before tensorboard logger to allow proper sync
-    if (opt['logger'].get('wandb') is not None) \
-        and (opt['logger']['wandb'].get('project') is not None) \
-        and ('debug' not in opt['name']):
+    if (
+        (opt["logger"].get("wandb") is not None)
+        and (opt["logger"]["wandb"].get("project") is not None)
+        and ("debug" not in opt["name"])
+    ):
 
-        assert opt['logger'].get('use_tb_logger') is True, ('should turn on tensorboard when using wandb')
+        assert (
+            opt["logger"].get("use_tb_logger") is True
+        ), "should turn on tensorboard when using wandb"
 
         init_wandb_logger(opt)
 
-
-    tb_logger = init_tb_logger(save_dir=hydra.core.hydra_config.HydraConfig.get().runtime.output_dir)
+    tb_logger = init_tb_logger(
+        save_dir=hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
+    )
 
     return tb_logger
-
-
-
